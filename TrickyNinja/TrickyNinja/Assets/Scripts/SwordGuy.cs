73e1f33f1a9e77df7ecec0a8613c14ddd9a0c6df
ScrambledEgge101 <minigraphicsguy@gmail.com>
2014-03-13 15:47:27 -0700

    Knockback Functionality
    Basic knockbacks with the sword guys- they only move backwards away from
    the player; they do not move up or down yet. Also, there is a knockback
    symbol identifier thingymajigger that says "Pow" and displays where the
    bullet dies. NOTE THAT I MODIFIED ONE OF DEVEN'S SCRIPTS WITH DEVEN'S
    PERMISSION IN ORDER TO MAKE THE POW SPAWN WHERE THE BULLET DIES. Also
    added a Pow Prefab to the game assets folder.

--- TrickyNinja/TrickyNinja/Assets/Scripts/SwordGuy.cs
+++ TrickyNinja/TrickyNinja/Assets/Scripts/SwordGuy.cs
@@ -1,19 +1,20 @@
-﻿using UnityEngine;
+﻿//SwordGuy Script by Jason Ege
+//Last edited on March 13, 2014
+
+using UnityEngine;
 using System.Collections;
 
 public class SwordGuy : EnemyScript {
 
 	public float fSpeed; //The speed of the SwordGuy, defined in the inspector interface.
+	public float fMaxSpeed; //The maximum speed allowed for the sword guy to accelerate to.
 	public Vector3 vOffset; //The spawning offset of the swordguy
 	
 	bool bGrounded;
-	float fNewPosY;
+	bool bBeenHit;
 
 	GameObject gPlayer;	//The player game object.
 	public float fAliveDistance; //How far away from the player the enemy has to be before he is destroyed to free up resources.
-	//bool bGoingUp; //Is the swordguy going up?
-	//float fKnockBackArc; //The arc by which the sword guy flies through the air when he gets knocked back.
-	//float fDeltaKnockBack; //The change in knockback arc.
 
 	// Use this for initialization
 	void Start () {
@@ -23,7 +24,7 @@ public class SwordGuy : EnemyScript {
 			fSpeed = -fSpeed;
 		}
 		bGrounded = false;
-		//fNewPosY = transform.position.y + 1.0f;
+		bBeenHit = false;
 	}
 	
 	//The Die method, derived from the "EntityScript".
@@ -35,98 +36,51 @@ public class SwordGuy : EnemyScript {
 	//The method that determines what happens when the player gets hurt.
 	public override void Hurt(int aiDamage)
 	{
-		Die ();
-		/*if (bGoingUp == true) //If the swordguy is moving up...
-		{
-			//He is immune to damage.
-		}
-		else if (bGoingUp == false) //If the sword guy is moving back down...
-		{
-			//fKnockBackArc = 1.0f;
-			//bGoingUp = true;
-			//fHealth -= aiDamage; //Damage the local game object.
-		}*/
+		rigidbody.AddForce (-fSpeed*2, 25.0f, 0.0f, ForceMode.Force);
+		bBeenHit = true;
+		fHealth--;
 	}
-	
-	/*void MoveAwayFromPlayer(Vector3 vMovementRight, Vector3 vMovementLeft)
-	{
-		//fDeltaKnockBack -= Time.deltaTime*2.0f;
-		//fKnockBackArc += fDeltaKnockBack;
-		if (transform.position.x > gPlayer.transform.position.x) //If the enemy is to the right of the player
-		{
-			rigidbody.AddForce (new Vector3(2.0f, 0.0f, 0.0f), ForceMode.Force);
-			//transform.Translate (vMovementRight, Space.World); //Move him away from the player a little.
-		}
-		else if (transform.position.x < gPlayer.transform.position.x) //If the enemy is to the left of the player
-		{
-			//transform.Translate (vMovementLeft, Space.World); //Move him away from the player a little.
-		}
-	}
-	*/
 	//The function that determines how the swordguy should move, derived from the "EntityScript" class.
 	public override void Move ()
 	{
-		/*if (bGoingUp == true) //If the swordguy is moving up...
+		//rigidbody.AddForce(transform.forward*fSpeed*Time.deltaTime, ForceMode.VelocityChange);
+		//rigidbody.detectCollisions = false;
+		//rigidbody.AddForce (0.0f, -9.8f*Time.deltaTime, 0.0f, ForceMode.VelocityChange); //Manually apply gravity
+		if (rigidbody.velocity.x >= fMaxSpeed*Time.deltaTime || rigidbody.velocity.x <= -fSpeed*Time.deltaTime)
 		{
-			//MoveAwayFromPlayer(new Vector3(-transform.right.z*30.0f*Time.deltaTime, 0.0f, 0.0f), new Vector3(transform.right.x*30.0f*Time.deltaTime, 0.0f, 0.0f));
-			//transform.position = new Vector3(transform.position.x, transform.position.y, 25.0f); //Set his Z position to stay at 25.
-			if (transform.position.y > vSpawnPoint.y+1.75f) //If the swordguy is above his spawn point.
-			{
-				bGoingUp = false; //He is no longer going up.
-			}
+			rigidbody.AddForce(new Vector3(fSpeed*Time.deltaTime, rigidbody.velocity.y, rigidbody.velocity.z), ForceMode.Acceleration);
 		}
-		if (transform.position.y > vSpawnPoint.y && bGoingUp == false) //If the swordguy is not going up, but he is still above his spawn point where he started...
+		else
 		{
-			//fDeltaKnockBack -= Time.deltaTime*2.0f;
-			//fKnockBackArc += fDeltaKnockBack;
-			//MoveAwayFromPlayer(new Vector3(-transform.right.z*30.0f*Time.deltaTime, fKnockBackArc, 0.0f), new Vector3(transform.right.x*30.0f*Time.deltaTime, fKnockBackArc, 0.0f));
-			//transform.Translate (0.0f, -fKnockBackArc, 0.0f);
-			//transform.Translate (0.0f, -6.0f * Time.deltaTime, 0.0f); //Move him down slightly.
+			rigidbody.AddForce(new Vector3(fSpeed*Time.deltaTime, 0.0f, 0.0f), ForceMode.Force);
 		}
-		if (transform.position.y < vSpawnPoint.y + 0.02f && bGoingUp == false) //If the sword guy is not in the air and he is below a spot really close to his spawn point...
-		{
-			//transform.position = new Vector3(transform.position.x, vSpawnPoint.y, 25.0f);
-			//fKnockBackArc = 0.0f;
-			//fDeltaKnockBack = 0.0f;
-			ChasePlayer (gPlayer, fSpeed); //The swordguy should chase the player.
-		}*/
-		//transform.Translate (0.0f, Mathf.Sin (-fKnockBackArc), 0.0f);
-		transform.Translate (0.0f, 0.0f, fSpeed*Time.deltaTime);
 		RaycastHit hit;
 		if (Physics.Raycast (transform.position, -transform.up, out hit, 3.0f))
 		{
-			if (hit.collider.tag == "Ground")
+			if (hit.collider.tag == "Ground" || hit.collider.tag == "Enemy")
 			{
 				bGrounded = true;
 			}
 			else
 			{
 				bGrounded = false;
+				//rigidbody.AddForce(0.0f, -9.8f, 0.0f, ForceMode.Force); //Manually apply gravity.
 			}
 		}
-		if (bGrounded == true)
+		if (bGrounded == true && transform.position.y <= 1.02f /*&& bBeenHit == false*/)
 		{
-			//transform.Translate (0.0f, 1.0f, 0.0f);
-			fNewPosY = hit.point.y + 1.0f;
-			transform.position = new Vector3(transform.position.x, fNewPosY, transform.position.z);
+			transform.position = new Vector3(transform.position.x, 1.02f, transform.position.z);
 		}
-		else if (bGrounded == false)
+		/*if (bBeenHit == true || bGrounded == false)
 		{
-			/*RaycastHit hit2;
-			if (Physics.Raycast(transform.position, -transform.up, out hit2, 3.0f))
-			{
-				if (hit2.collider.tag == "Ground")
-				{
-					fNewPosY = hit2.point.y + 1.5f;
-					bGrounded = true;
-				}
-			}*/
-			//fNewPosY = 1.02f;
-			//transform.Translate (0.0f, 1.0f, 0.0f);
-			//transform.position = new Vector3(transform.position.x, fNewPosY, transform.position.z);
-			transform.Translate (0.0f, -Time.deltaTime, 0.0f);
-			//fNewPosY -= 9.8f*Time.deltaTime;
+			rigidbody.AddForce(0.0f, 18.0f, 0.0f, ForceMode.VelocityChange);
 		}
+		if (bBeenHit == true)
+		{
+			bGrounded = false;
+			bBeenHit = false;
+			rigidbody.AddForce (0.0f, 18.0f, 0.0f, ForceMode.VelocityChange);
+		}*/
 		DistanceKill();
 	}
 	
@@ -141,7 +95,7 @@ public class SwordGuy : EnemyScript {
 	// Update is called once per frame
 	void Update () {
 		Move (); //Move the sword guy in his own special way.
-		if (fHealth < 0) //If the swordguy is dead...
+		if (fHealth <= 0) //If the swordguy is dead...
 		{
 			Die (); //The swordguy should die.
 		}
