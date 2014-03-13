b064818bc87c5e85ac673c8db61102f08c467f76
ScrambledEgge101 <minigraphicsguy@gmail.com>
2014-03-13 15:55:34 -0700

    Backtrack
    I reverted back to the one that existed before I merged with the current
    master because the current master screwed up my scene!

--- TrickyNinja/TrickyNinja/Assets/Scripts/ShadowScript2.cs
+++ TrickyNinja/TrickyNinja/Assets/Scripts/ShadowScript2.cs
@@ -29,7 +29,6 @@ public class ShadowScript2 : EntityScript
 	public GameObject gPlayerAttackPrefab;
 	public GameObject goCharacter;
 	public GameObject goSwordPivot;
-	//public GameObject goRopePivot;
 	public GameObject goNaginataPivot;
 
 	bool bMeleeAttack = false;
@@ -254,7 +253,7 @@ public class ShadowScript2 : EntityScript
 		}
 		if(bRopeAttack)
 		{
-			//do nothing the player handles ropes
+
 		}
 		if(bNaginataAttack)
 		{
