46783cd84ec7beac61a0971d6ba70f7cd6f16906
ScrambledEgge101 <minigraphicsguy@gmail.com>
2014-03-14 11:56:40 -0700

    SwordGuy Jumps back! Finally!
    The Sword Guy now jumps back when hit and is invinsible when in the air.

--- TrickyNinja/TrickyNinja/Assets/Scripts/SwordGuy.cs
+++ TrickyNinja/TrickyNinja/Assets/Scripts/SwordGuy.cs
@@ -1,5 +1,5 @@
 ﻿//SwordGuy Script by Jason Ege
-//Last edited on March 13, 2014
+//Last edited on March 14, 2014
 
 using UnityEngine;
 using System.Collections;
@@ -8,6 +8,8 @@ public class SwordGuy : EnemyScript {
 
 	public float fSpeed; //The speed of the SwordGuy, defined in the inspector interface.
 	public float fMaxSpeed; //The maximum speed allowed for the sword guy to accelerate to.
+	public float fHorizontalKnockBack; //The amount of force the sword guy gets thrown back when he is hit.
+	public float fVerticalKnockBack; //The amount of force the sword guy gets thrown into the air when he is hit.
 	public Vector3 vOffset; //The spawning offset of the swordguy
 	
 	bool bGrounded;
@@ -36,51 +38,57 @@ public class SwordGuy : EnemyScript {
 	//The method that determines what happens when the player gets hurt.
 	public override void Hurt(int aiDamage)
 	{
-		rigidbody.AddForce (-fSpeed*2, 25.0f, 0.0f, ForceMode.Force);
+		RaycastHit hit2;
+		if (Physics.Raycast (transform.position, -transform.up, out hit2, 1.0f))
+		{
+			if (hit2.collider.tag == "Ground")
+			{
+				if (EnemyIsRightOfPlayer(gPlayer))
+				{
+					if (rigidbody.velocity.x < 0.0f)
+					{
+						rigidbody.velocity = new Vector3(fHorizontalKnockBack, fVerticalKnockBack, rigidbody.velocity.z);
+						fHealth -=  aiDamage;
+					}
+				}
+				else if (!EnemyIsRightOfPlayer(gPlayer))
+				{
+					if (rigidbody.velocity.x > 0.0f)
+					{
+						rigidbody.velocity = new Vector3(-fHorizontalKnockBack, fVerticalKnockBack, rigidbody.velocity.z);
+						fHealth -= aiDamage;
+					}
+				}
+			}
+		}
 		bBeenHit = true;
-		fHealth--;
 	}
 	//The function that determines how the swordguy should move, derived from the "EntityScript" class.
 	public override void Move ()
 	{
-		//rigidbody.AddForce(transform.forward*fSpeed*Time.deltaTime, ForceMode.VelocityChange);
-		//rigidbody.detectCollisions = false;
-		//rigidbody.AddForce (0.0f, -9.8f*Time.deltaTime, 0.0f, ForceMode.VelocityChange); //Manually apply gravity
-		if (rigidbody.velocity.x >= fMaxSpeed*Time.deltaTime || rigidbody.velocity.x <= -fSpeed*Time.deltaTime)
-		{
-			rigidbody.AddForce(new Vector3(fSpeed*Time.deltaTime, rigidbody.velocity.y, rigidbody.velocity.z), ForceMode.Acceleration);
-		}
-		else
-		{
-			rigidbody.AddForce(new Vector3(fSpeed*Time.deltaTime, 0.0f, 0.0f), ForceMode.Force);
-		}
 		RaycastHit hit;
-		if (Physics.Raycast (transform.position, -transform.up, out hit, 3.0f))
+		if (Physics.Raycast (transform.position, -transform.up, out hit, 1.02f))
 		{
 			if (hit.collider.tag == "Ground" || hit.collider.tag == "Enemy")
 			{
 				bGrounded = true;
+				if (rigidbody.velocity.x >= fMaxSpeed*Time.deltaTime || rigidbody.velocity.x <= -fMaxSpeed*Time.deltaTime)
+				{
+				}
+				else
+				{
+					rigidbody.AddForce(new Vector3(fSpeed*Time.deltaTime, 0.0f, 0.0f), ForceMode.Force);
+				}
 			}
 			else
 			{
 				bGrounded = false;
-				//rigidbody.AddForce(0.0f, -9.8f, 0.0f, ForceMode.Force); //Manually apply gravity.
 			}
 		}
 		if (bGrounded == true && transform.position.y <= 1.02f /*&& bBeenHit == false*/)
 		{
 			transform.position = new Vector3(transform.position.x, 1.02f, transform.position.z);
 		}
-		/*if (bBeenHit == true || bGrounded == false)
-		{
-			rigidbody.AddForce(0.0f, 18.0f, 0.0f, ForceMode.VelocityChange);
-		}
-		if (bBeenHit == true)
-		{
-			bGrounded = false;
-			bBeenHit = false;
-			rigidbody.AddForce (0.0f, 18.0f, 0.0f, ForceMode.VelocityChange);
-		}*/
 		DistanceKill();
 	}
 	
