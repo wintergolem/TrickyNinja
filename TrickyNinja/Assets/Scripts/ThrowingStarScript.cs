a1c2792cd0f449c89851dd2ea49d5aee46a52f57
DevenSmith <Deven.smith@students.aie.edu.au>
2014-03-27 16:34:01 -0700

    3-27-2014 1630

---
+++ TrickyNinja/Assets/Scripts/ThrowingStarScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrowingStarScript : MonoBehaviour 
+{
+	float fSpin = 1.0f;
+	public float fSpinSpeed = 10.0f;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Rotate(new Vector3(0.0f, 0.0f, fSpin*fSpinSpeed*Time.deltaTime), Space.World);
+	}
+}
