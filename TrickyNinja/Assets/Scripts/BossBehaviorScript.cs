83be34c172ab1eb5f5e34b65c75f3f56e007c8e7
wintergolem <wintergolem@gmail.com>
2014-05-15 13:54:05 -0700

    Dragon Fix on Steven2 scene

--- TrickyNinja/Assets/Scripts/BossBehaviorScript.cs
+++ TrickyNinja/Assets/Scripts/BossBehaviorScript.cs
@@ -7,23 +7,30 @@ using System.Collections;
 public class BossBehaviorScript : MonoBehaviour {
 
 	public float fTriggerDistance = 3;
-	bool bStartRise = false;
 
 	public float fStartZ = 0;
 	public float fEndZ = 10;
+	public float fRiseSpeed = 5;
+
 	public bool bRisen = false;
 	public bool bFall = false;
-	public float fRiseSpeed = 5;
-	GameManager manager;
 
 	public GameObject goActivePlayer;
+	public GameObject goDragon;
 	public CameraShake cCamera;
-	public GameObject[] agoEyes;
+	//public GameObject[] agoEyes;
 	public ParticleSystem expl;
+
+	bool bStartRise = false;
+	bool bExplPlayed = false;
+	
+	GameManager manager;
+
 	// Use this for initialization
 	void Start () {
-		foreach( GameObject goE in agoEyes )
-			goE.SetActive( false );
+		goDragon.SetActive(false);
+//		foreach( GameObject goE in agoEyes )
+//			goE.SetActive( false );
 	}
 	
 	// Update is called once per frame
@@ -32,22 +39,26 @@ public class BossBehaviorScript : MonoBehaviour {
 		if( bStartRise || Mathf.Abs(goActivePlayer.transform.position.x - transform.position.x )< fTriggerDistance) //Mathf.Abs( Vector3.Distance( goActivePlayer.transform.position , transform.position ) )< fTriggerDistance ) 
 		{
 			bStartRise = true;
-			if( !expl.isPlaying )
+			if( !expl.isPlaying && !bExplPlayed)
+			{
+				goDragon.SetActive(true);
 				expl.Play();
+				bExplPlayed = true;
+			}
 		}
 		if( !bRisen && bStartRise )
 		{
 			//transform.Translate( transform.forward * Time.deltaTime * fRiseSpeed, Space.World );
 			cCamera.Shake();
-			//if( transform.position.z >= fEndZ ) 
-			//{
-			Vector3 temp = transform.position;
-			temp.z = fEndZ;
-			transform.position = temp;
-			bRisen = true;
-			foreach( GameObject goE in agoEyes )
-				goE.SetActive( true );
-			//}
+			if( transform.position.z >= fEndZ ) 
+			{
+				Vector3 temp = transform.position;
+				temp.z = fEndZ;
+				transform.position = temp;
+				bRisen = true;
+	//			foreach( GameObject goE in agoEyes )
+	//				goE.SetActive( true );
+			}
 		}
 		if( bFall )
 		{
